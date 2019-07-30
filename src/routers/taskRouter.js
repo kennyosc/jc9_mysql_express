389084363f5e16a261504eb6160397043576bede
kennyosc <kennyoscar95@gmail.com>
2019-07-30 10:20:01 +0700

    add update and delete task route

--- src/routers/taskRouter.js
+++ src/routers/taskRouter.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const conn = require('../connection/index.js')
 
+//POST 1 TASKS
 router.post('/tasks', (req,res)=>{
     const sql = `INSERT INTO tasks SET ?`
     const data = req.body
@@ -25,3 +26,42 @@ router.post('/tasks', (req,res)=>{
         })
     })
 })
+
+//READ ALL TASK BY ID
+router.get('/users/:user_id/tasks', (req,res)=>{
+    const sql = `SELECT * FROM tasks WHERE user_id = ${req.params.user_id}`
+
+    conn.query(sql, (err,results)=>{
+        if(err){
+            return res.send(err)
+        }
+
+        res.send(results)
+    })
+})
+
+//UPDATE TASK BY ID
+router.patch('/users/tasks/:id', (req,res)=>{
+    const sql = `UPDATE tasks SET completed = true WHERE id = ${req.params.id}`
+
+    conn.query(sql, (err,results)=>{
+        if(err){
+            return res.send(results)
+        }
+        res.send(results)
+    })
+})
+
+//DELETE TASK BY ID
+router.delete('/users/:user_id/tasks/:id',(req,res)=>{
+    const sql = `DELETE FROM tasks WHERE user_id = ${req.params.user_id} AND id = ${req.params.id}`
+
+    conn.query(sql, (err,results)=>{
+        if(err){
+            return res.send(err)
+        }
+
+        res.send(results)
+    })
+})
+module.exports = router
