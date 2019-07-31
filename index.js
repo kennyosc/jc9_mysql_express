f01e5fb21ca2acdc0674420a3bcd40ed9e897190
kennyosc <kennyoscar95@gmail.com>
2019-07-31 10:22:56 +0700

    fix server bug

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const express = require('express')
-const app = express()
+const server = express()
 const mysql = require('mysql')
-const port2 = require('./src/config/port.js')
 
 //process.env.PORT adalah port yang nantinya diberikan oleh heroku secara otomatis
 // klau tidak ada port dari tempat lainnya, maka akan dijalankan 2019
-const port = port2
+const port = require('./src/config/port.js')
 
 const userRouter = require('./src/routers/userRouter.js')
 const taskRouter = require('./src/routers/taskRouter.js')
 
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
+server.use(express.json())
+server.use(userRouter)
+server.use(taskRouter)
 
 
 server.listen(port, ()=>{
     console.log('Connected to port ' + port)
 })
+
+/*
+UPLOAD HEROKU
+1. heroku login
+2. heroku create <nama project>
+3. git add .
+4. git commit -m ''
+5. git push origin master
+6. git push heroku master
+ */
